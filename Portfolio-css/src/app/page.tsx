971797db5a54a2b3ed64c2075a58aad38852996f
Aqsa khan <161198603+Aqlish@users.noreply.github.com>
2024-11-11 00:14:52 +0500

    Update page.tsx

--- Portfolio-css/src/app/page.tsx
+++ Portfolio-css/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import najaf from "../../image/najaf.png";
+import aqsa from "../../image/aqsa.jpg";
 export default function home(){
   return(
     <div>
@@ -9,7 +9,7 @@ export default function home(){
       {/* start main section div */}
       <div>
       <div style={{paddingLeft:"70px",height:"40px",background:"#2a2b2b",marginLeft:"15%",marginRight:"15%",display:"flex",gap:"10%",color:"white", paddingTop:"8px"}}>
-         <div><strong  ><b style={{color:"red"}}>Naj</b>af</strong></div>
+         <div><strong  ><b style={{color:"red"}}>Aqsa</b>Khan</strong></div>
          <div><a href="https://multipagenextjscss.vercel.app/">MPNcSS</a></div>
          <div><a href="https://multipagenextjstailwind.vercel.app/">MPNtailwind</a></div>
          <div><a href="https://portfoliocss-dusky.vercel.app/">PortFcss</a></div>
@@ -18,8 +18,8 @@ export default function home(){
          </div>
       <div style={{background:"white",marginLeft:"15%",marginRight:"15%",height:"20%",display:"flex",gap:"30%"}}>
         <div style={{paddingLeft:"100px",paddingTop:"50px",paddingBottom:"50px",fontSize:"100%"}}>
-          <strong >Hi < br></br>I am<strong style={{color:"orange"}}> Najaf Ali</strong> <br></br> a Frontend Developer</strong>  <br></br>
-          <p>I am Najaf Ali, a UI/UX designer & front-end web developer. Focused on creating unique,
+          <strong >Hi < br></br>I am<strong style={{color:"orange"}}> Aqsa</strong> <br></br> a Frontend Developer</strong>  <br></br>
+          <p>I am Aqsa khan, a UI/UX designer & front-end web developer. Focused on creating unique,
              results-driven digital experiences through clean and functional design.r</p>
              <br></br> <br></br>
              <strong style={{background:"black",color:"white",marginRight:"5%",borderStyle:"solid",borderRadius:"5px",padding:"6px"}}>Hire Me</strong>
@@ -27,7 +27,7 @@ export default function home(){
 
           </div>
         <div>
-          <Image src={najaf} alt="" width="1500" height="1500" style={{paddingRight:"30px",paddingTop:"30px"}} />
+          <Image src={aqsa} alt="" width="1500" height="1500" style={{paddingRight:"30px",paddingTop:"30px"}} />
         </div>
 
       </div>
