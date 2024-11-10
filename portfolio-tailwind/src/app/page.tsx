b7f5e999baaabe8780f7c5cfbf97acf3a728f4f3
Aqsa khan <161198603+Aqlish@users.noreply.github.com>
2024-11-11 00:11:11 +0500

    Update page.tsx

--- portfolio-tailwind/src/app/page.tsx
+++ portfolio-tailwind/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image"
-import najaf from "../../image/najaf.png"
+import aqsa from "../../image/aqsa.jpg"
 export default function home(){
   return(
     <div>
@@ -23,9 +23,9 @@ export default function home(){
         {/* style={{background:"white",marginLeft:"15%",marginRight:"15%",height:"20%",display:"flex",gap:"30%" */}
         <div className="pl-[100px] pt-[50px] pb-[50px] text-[100%] w-[80%]" >
         {/* paddingLeft:"100px",paddingTop:"50px",paddingBottom:"50px",fontSize:"100%" */}
-          <strong style={{animation: "fadeZoom 2s infinite alternate"}} >Hi < br></br>I am<strong style={{color:"orange"}}> Najaf Ali</strong> <br></br> a Frontend Developer</strong>  <br></br>
-          <p>I am Najaf Ali, a UI/UX designer & front-end web developer. Focused on creating unique,
-             results-driven digital experiences through clean and functional design.The visionary founder of nysquiz, Najaf ALi is the creative mind
+          <strong style={{animation: "fadeZoom 2s infinite alternate"}} >Hi < br></br>I am<strong style={{color:"orange"}}> Aqsa</strong> <br></br> a Frontend Developer</strong>  <br></br>
+          <p>I am Aqsa khan, a UI/UX designer & front-end web developer. Focused on creating unique,
+             results-driven digital experiences through clean and functional design.The visionary founder of nysquiz, Aqsa is the creative mind
              behind the platform, making interactive learning accessible through AI-generated quizzes tailored to user preferences.
              </p>
              <br></br> <br></br>
@@ -36,7 +36,7 @@ export default function home(){
 
           </div>
         <div>
-          <Image src={najaf} alt="" width="500" height="500" style={{paddingRight:"30px",paddingTop:"30px"}} />
+          <Image src={aqsa} alt="" width="500" height="500" style={{paddingRight:"30px",paddingTop:"30px"}} />
         </div>
 
       </div>
